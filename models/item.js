5cb9da78994c6f1752abdf73b39f28cd07c83dda
Joel <joel.salminen@gmail.com>
2018-05-07 16:25:42 +0300

    Added item schema

---
+++ models/item.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create item schema and model
+const ItemSchema = new Schema({
+	name: {
+		type: String, 
+		required: [true, 'Name field is required']
+	},
+	serial_number: {
+		type: Number,
+		required: [true, 'Serial number is required']
+	},
+	location: {
+		type: String
+	},
+	expiration: {
+		type: Date
+	}
+
+});
+
+const Item = mongoose.model('item', ItemSchema);
+
+module.exports = Item;
