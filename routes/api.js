835eb81be1ae4c8a05258cc690f91bfa5b639502
Joel <joel.salminen@gmail.com>
2018-05-07 16:00:49 +0300

    Added server.js and routes.js

---
+++ routes/api.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+// setting up the router
+const router = express.Router();
+
+router.get('/items', (req, res)=>{
+	res.send({express: 'Guten Tag'});
+});
+
+router.post('/items', (req, res)=>{
+	//
+});
+
+
+// router.put(()=>{
+
+// });
+
+// router.delete(()=>{
+
+// });
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use('/api', require('./routes/api'));
+
+
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`)
+});
+
+
