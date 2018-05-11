2912a816cef43122ac18d75e12874a24306b8f19
Joel <joel.salminen@gmail.com>
2018-05-11 16:54:53 +0300

    Body-parser now works with postman and ajax requests

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // parsing data that was received
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/api', require('./routes/api'));
 
