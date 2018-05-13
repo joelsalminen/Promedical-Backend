a7fda7f9648d22dbc95973b30a3b8259b6eee62a
Joel <joel.salminen@gmail.com>
2018-05-13 14:54:06 +0300

    Lending model added

---
+++ models/lending.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//create lending schema and model
+const LendingSchema = new Schema({
+
+	lender: {
+		type: String,
+		required: [true, 'Lender field is required']
+	},
+	Customer: {
+		type: Object,
+		required: [true, 'Customer object is required']
+		name: {
+			type: String
+			required: [true, 'Customer name field is required']
+		},
+		contactInfo: {
+			type: String
+		}
+	},
+	startDate: {
+		type: String,
+		required: [true, 'Starting date field is required']
+	},
+	returnDate: {
+		type: String, 
+		required: [true, 'Return date field is required']
+	},
+	lendType: {
+		type: String,
+		required: [true, 'Lend type field is required']
+	},
+	price: {
+		type: Double,
+	}
+
+	item: {
+		name: String,
+		required: [true, 'Item name field is required']
+	}
+	serial: {
+		type: Number,
+		required: [true, 'Serial field is required']
+	}
+
+});
+
+const Lending = mongoose.model('lending', LendingSchema);
+
+module.exports = Lending;
