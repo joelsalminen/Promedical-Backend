24f9ec96e22f45993e38ab9a2accd7e5c3ab9a53
joelsalminen <joel.salminen@gmail.com>
2018-05-30 15:57:06 +0300

    Signup now responds with token

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,4 @@
+const JWT = require('jsonwebtoken');
 const User = require('../models/user');
 
 module.exports = {
@@ -11,8 +12,14 @@ module.exports = {
 		/*  Create new user */
 		User.create(req.value.body).
 			then(user=>{
-				/* respond with token */
-				res.send({token: 'token'});
+				/* Respond with token */
+				const token = JWT.sign({
+					iss: 'Promedical',
+					sub: user._id,
+					iat: new Date().getTime(),
+					exp: new Date().setDate(new Date().getDate() + 1)
+				}, 'uiiroDg8dDs9L59fj89fFDDs4dgGAgg0jsHsjgP44gg0');
+				res.send({token});
 			}).catch(next);
 
 	},
