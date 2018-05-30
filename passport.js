f671a2ff33398451f3967ae63d196487a97a5123
joelsalminen <joel.salminen@gmail.com>
2018-05-30 16:29:41 +0300

    Installed bcrypt package.

--- passport.js
+++ passport.js
@@ -1,10 +1,11 @@
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const { ExtractJwt } = require('passport-jwt');
+const LocalStrategy = require('passport-local').Strategy;
 const { JWT_SECRET } = require('./configuration');
 const User = require('./models/user') ;
 
-// json web tokens strategy
+// JSON web tokens strategy
 passport.use(new JwtStrategy({
 	jwtFromRequest: ExtractJwt.fromHeader('authorization'),
 	secretOrKey: JWT_SECRET
@@ -24,3 +25,32 @@ passport.use(new JwtStrategy({
 		done(error, false);
 	}
 }));
+
+
+// Local Strategy
+passport.use(new LocalStrategy({
+	usernameField: 'email'
+}, async (email, password, done)=>{ 
+	try{
+		// Find the user given the email
+		const user = await User.findOne({email});
+
+		// If user doesn't exist, handle it
+		if(!user){
+			return done(null, false);
+		}
+
+		// Check if password is correct
+		const isMatch = await user.isValidPassword(password);
+		if(!isMatch){
+			return done(null, false);
+		}
+
+		// Otherwise, return the user
+		done(null, user);
+	} catch(error){
+		done(error, false);
+	}
+
+}));
+
