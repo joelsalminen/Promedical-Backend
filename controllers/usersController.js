aedda229fe06173ebf4c3bdeb1a854de73e35cd2
joelsalminen <joel.salminen@gmail.com>
2018-05-30 15:23:43 +0300

    Signup functionality added (doesn't respond with a token yet).

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,10 +1,25 @@
+const User = require('../models/user');
+
 module.exports = {
+	signup: async (req, res, next) => {
+		/* Check if there is a user with the same email */
+		const foundUser = await User.findOne({email: req.value.body.email});
+		if (foundUser) {
+			return res.status(403).json({error: "Email is already in use."});
+		}
+
+		/*  Create new user */
+		User.create(req.value.body).
+			then(user=>{
+				/* respond with token */
+				res.send({token: 'token'});
+			}).catch(next);
+
+	},
+
 	login: async (req, res, next) =>{
 		//generate token
 		console.log("login called");
-	},
-	signup: async (req, res, next) => {
-		// email and password
-		console.log("signup called");
 	}
+
 }
