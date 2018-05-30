66942bd20d2fe9a90877f5aacf413153f5e84477
joelsalminen <joel.salminen@gmail.com>
2018-05-30 10:16:21 +0300

    User model added

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongose');
+//const bcrypt = require('bcryptjs');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+	email:{
+		type: String,
+		required: true,
+		unique: true,
+		lowercase: true
+	},
+	password: {
+		type: String,
+		required: true
+	}
+});
+
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
